1bbf3dd236baa2190afa9f6b196b2c9a9aa9a769
muskan <itsmuskancorp@gmail.com>
2023-06-16 19:36:01 +0530

    add comments

--- static/index.js
+++ static/index.js
@@ -1,37 +1,36 @@
+ //this function takes two arguments,the input and an array of possible autocompleted values/meals
 function autocomplete(inp, arr) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
+   
     var currentFocus;
-    /*execute a function when someone writes in the text field:*/
+    //listener when someone types something
     inp.addEventListener("input", function(e) {
         var a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/
+        //close already open dropdown list
         closeAllLists();
         if (!val) { return false;}
         currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
+        //clone DIV element that will contain the available meals
         a = document.createElement("DIV");
         a.setAttribute("id", this.id + "autocomplete-list");
         a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
-        /*for each item in the array...*/
+        //for loop to display all elemets that are matching with input
         for (i = 0; i < arr.length; i++) {
-          /*check if the item starts with the same letters as the text field value:*/
+          //check if the item starts with the same letters as the inoput
           if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/
+            //create a DIV element for each matching meals
             b = document.createElement("DIV");
-            /*make the matching letters bold:*/
+            //highlight(bold) the matching letters
             b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
             b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/
+            //insert a input field that will hold the current array item's value:*/
             b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/
+            //execute a function when someone clicks on value in the dropdown(DIV element)
             b.addEventListener("click", function(e) {
-                /*insert the value for the autocomplete text field:*/
+                //insert the value for the autocomplete text field
                 inp.value = this.getElementsByTagName("input")[0].value;
-                /*close the list of autocompleted values,
-                (or any other open lists of autocompleted values:*/
+                //close the list of autocompleted values,
+                //(or any other open lists of autocompleted values
                 closeAllLists();
             });
             a.appendChild(b);
